aac3883263ff29334134f496440e7e7a1221868c
Adil Alam <adilalamadil806@gmail.com>
2026-04-16 18:16:37 +0530

    Fix component import and rendering in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import SocialApp from "./App.jsx";
+<SocialApp />
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <App />
